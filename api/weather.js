65f736c4ecd9801301cf6e3b2c6f7ae62bb2edee
Lao-Yeh <411171110@mail.nknu.edu.tw>
2025-06-23 23:20:17 +0800

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,6 +1,8 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
   const { location } = req.query;
-  const apiKey = 'CWA-34476997-F821-45C5-8BDE-8F5F7A4E6609'; // 你的中央氣象局 API Key
+  const apiKey = 'CWA-34476997-F821-45C5-8BDE-8F5F7A4E6609';
   const url = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-001?Authorization=${apiKey}&locationName=${encodeURIComponent(location)}`;
   const response = await fetch(url);
   const data = await response.json();
